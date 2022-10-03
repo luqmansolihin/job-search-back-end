cd77c46fcb22fc2b5e0d910458c9a263740421d1
luqmansolihin <luqmansolihin.jte@gmail.com>
2022-10-03 15:23:59 +0700

    update route service provider

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -30,10 +30,12 @@ class RouteServiceProvider extends ServiceProvider
 
         $this->routes(function () {
             Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+                ->prefix('api/v1')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/api/v1/api.php'));
 
             Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
