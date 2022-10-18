32265fb58569f3a84f4cbb890be3c0819ccf2124
luqmansolihin <luqmansolihin.jte@gmail.com>
2022-10-18 13:53:04 +0700

    update config

--- config/rc.php
+++ config/rc.php
@@ -1,5 +1,24 @@
 <?php
 
 return [
-    'not_found' => ['rc' => '404', 'message' => 'Not Found', 'data' => false]
+    'ok' => [
+        'rc' => '0000',
+        'message' => 'Ok',
+        'data' => false
+    ],
+    'bad_request' => [
+        'rc' => '0101',
+        'message' => 'Bad Request',
+        'data' => false
+    ],
+    'unauthorized' => [
+        'rc' => '0201',
+        'message' => 'Unauthorized',
+        'data' => false
+    ],
+    'not_found' => [
+        'rc' => '0301',
+        'message' => 'Not Found',
+        'data' => false
+    ]
 ];
