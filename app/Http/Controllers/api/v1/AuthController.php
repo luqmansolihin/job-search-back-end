3c9459d6b6ace0bde3b03270ad3c74eed2e9bad3
luqmansolihin <luqmansolihin.jte@gmail.com>
2022-12-02 09:34:45 +0700

    Create Auth Controller

---
+++ app/Http/Controllers/api/v1/AuthController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\api\v1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Traits\ApiResponses;
+use Exception;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Http\JsonResponse;
+
+class AuthController extends Controller
+{
+    use ApiResponses;
+
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'refresh']]);
+    }
+
+    public function login(LoginRequest $request): JsonResponse
+    {
+        if (!$token = auth()->attempt($request->validated())) {
+            throw new AuthenticationException;
+        }
+
+        $user = auth()->user()->load('roles')->toArray();
+
+        return $this->successWithTOken($token, $user);
+    }
+
+    public function refresh(): JsonResponse
+    {
+        $token = auth()->refresh(true, true);
+
+        return $this->successWithTOken($token);
+    }
+
+    public function logout(): JsonResponse
+    {
+        auth()->logout(true);
+
+        return $this->success();
+    }
+}
