bec08b3efd91e44625672659be83ff661709eaf5
luqmansolihin <luqmansolihin.jte@gmail.com>
2022-12-02 09:31:09 +0700

    Update authenticate middleware to throw Authentication Exception when unauthenticated

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,20 +2,24 @@
 
 namespace App\Http\Middleware;
 
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
+use Illuminate\Http\Request;
 
 class Authenticate extends Middleware
 {
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return string|null
+     * @param Request $request
+     * @return void
+     * @throws AuthenticationException
      */
-    protected function redirectTo($request)
+    protected function redirectTo($request): void
     {
-        if (! $request->expectsJson()) {
-            return route('login');
+        if (!$request->expectsJson()) {
+            // return route('login');
+            throw new AuthenticationException;
         }
     }
 }
